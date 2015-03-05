6b4bc486b1a29c689a36ed50e9661fcc33625136
ahbuss <ahbuss@172.20.148.230>
2015-03-04 16:30:01 -0800

    Added some code

---
+++ src/test/HelloGit.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author ahbuss
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello from Git!");
+    }
+
+}
