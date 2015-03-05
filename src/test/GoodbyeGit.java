74e61871fb930d15509187adadfae0802452eb4b
ahbuss <ahbuss@172.20.148.230>
2015-03-04 16:34:08 -0800

    Added more code to branch

---
+++ src/test/GoodbyeGit.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ *
+ * @author ahbuss
+ */
+public class GoodbyeGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Goodbye Git!");
+    }
+
+}
